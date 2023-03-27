d4a2777fa2db39a5d435be2a9c9a65460ff2a51f
KSUMotorsport <ksumotorsport@gmail.com>
2023-03-27 14:08:17 -0400

    modified:   include/SPI_handle.h 	modified:   src/SPI_handle.cpp

--- src/SPI_handle.cpp
+++ src/SPI_handle.cpp
@@ -2,19 +2,19 @@
 
 #include "TempSensor.h"
 #include "SPI_handle.h"
+#include "CAN_MCP2515.h"
 
 
 // ------------------------------------------------
 // Initializes the SPI pins, mode, and CS
 
+
 void init_SPI()
 {
 
-    SPI.begin();
+    CAN_MCP2515(CHIPSELECT);
 
-    SPI.beginTransaction(SPISettings(9600, LSBFIRST, SPI_MODE0));
-
-    pinMode(CHIPSELECT, OUTPUT);
+    CAN.begin(500000);
 
 }
 
@@ -25,61 +25,20 @@ void init_SPI()
 void send_SPI(uint32_t id, uint8_t buf[])
 {
 
-    SPI_message_t msg;
+    CAN_Frame msg;
 
-    uint64_t buffer;
-
-    msg.id |= id;
-
-
+    msg.id = id;
 
     // Load buffer with Temperature array 
 
     for (uint8_t INDEX = 0; INDEX < sizeof(buf); INDEX++)
     {
 
-        msg.buf[INDEX] = buf[INDEX];
+        msg.data[INDEX] = buf[INDEX];
 
     }
 
-    for (int INDEX = msg.len; INDEX > 0; INDEX--)
-    {
-
-        buffer |= (msg.buf[INDEX] << ((INDEX * 8) - 8));
-
-    }
-
-
-
-    // SPI.beginTransaction(SPISettings(9600, LSBFIRST, SPI_MODE0));
-
-    // enable CAN controller to recieve data
-
-    digitalWrite(CHIPSELECT, LOW);
-
-    // transfer data
-
-
-    // for (int i = 0; i < msg.len; i++)
-    // {
-
-    //     Serial.print("buffer ");
-    //     Serial.print(i);
-    //     Serial.print(": ");
-    //     Serial.println(msg.buf[i]);
-
-    // }
-
-    // transfer identity
-
-    SPI.transfer(msg.buf, msg.len);
-
-    SPI.transfer(msg.len);
-    SPI.transfer(msg.id);
-
-    SPI.endTransaction();
-
-    digitalWrite(CHIPSELECT, HIGH);
+    CAN.write(msg);
 
 }
 
